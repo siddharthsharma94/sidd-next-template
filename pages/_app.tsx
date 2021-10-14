41e33b482fd569fbae1476d7f8681a060c4d445a
Siddharth Sharma <sharmasiddharthcs@gmail.com>
2021-10-14 10:24:47 -0700

    Hide Navbar on Pages, Decouple components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Navbar from "../components/Navbar"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-auto">
+      <Navbar />
       <Component {...pageProps} />
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Banner from "../components/Banner"
 import Features from "../components/Features"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
@@ -5,6 +6,7 @@ import Hero from "../components/Hero"
 const Home = () => {
   return (
     <div>
+      <Banner />
       <Hero />
       <Features />
       <Footer />
